ce899b09c5b808ad68a1cfa87b60f04c3c59ccb4
Oissevalt <57583509+oissevalt@users.noreply.github.com>
2025-12-06 03:17:22 +0000

    EchoBack: filter CQ components and customizable probability

--- src/echoback/echoback.ts
+++ src/echoback/echoback.ts
@@ -14,6 +14,9 @@ const EXT_NAME = "echoback";
 const EXT_VERSION = "1.0.0";
 const EXT_AUTHOR = "檀轶步棋";
 
+const CONF_CHANCE_KEY = "EchoBack:Probability";
+const CONF_CHANCE_DEF = 0.15;
+
 // Global variables
 
 const GlobalState: { [groupId: string]: { active: boolean, content: string, counter: number } } = {};
@@ -21,9 +24,11 @@ const GlobalState: { [groupId: string]: { active: boolean, content: string, coun
 const Extension = getOrRegisterExtension();
 
 const ControlCommand = seal.ext.newCmdItemInfo();
+
 ControlCommand.name = "echoback";
 ControlCommand.help = ".echoback on/off";
 ControlCommand.disabledInPrivate = true;
+
 ControlCommand.solve = (context, message, argument) => {
     const executionResult = seal.ext.newCmdExecuteResult(true);
 
@@ -49,14 +54,18 @@ ControlCommand.solve = (context, message, argument) => {
         case "on":
             GlobalState[groupId].active = true;
             GlobalState[groupId].counter = -1;
+
             setExtensionActive(groupId, true);
             seal.replyToSender(context, message, "EchoBack 已开启");
+
             break;
         case "off":
             GlobalState[groupId].active = false;
             GlobalState[groupId].counter = -1;
+
             setExtensionActive(groupId, false);
             seal.replyToSender(context, message, "EchoBack 已关闭");
+
             break;
         default:
             executionResult.showHelp = true;
@@ -86,19 +95,33 @@ Extension.onNotCommandReceived = (context, message) => {
 
     switch (GlobalState[groupId].counter) {
         case -1:
-            if (Math.random() < 0.15) {
+            if (Math.random() < seal.ext.getFloatConfig(Extension, CONF_CHANCE_KEY)) {
+                if (!message.message || hasCqComponent(message.message)) {
+                    console.log(`EchoBack: Message has a CQ component, skipped.`);
+                    break;
+                }
+
                 GlobalState[groupId].content = message.message;
                 GlobalState[groupId].counter = Math.floor(Math.random() * 26) + 15;
+
                 console.log(`EchoBack: Will echo lastly received message in ${GlobalState[groupId].counter} messages.`);
             }
             break;
         case 0:
             seal.replyToSender(context, message, `${GlobalState[groupId].content}`);
+
             GlobalState[groupId].counter = -1;
             GlobalState[groupId].content = "";
+
             break;
         default:
+            if (GlobalState[groupId].counter < 0) {
+                GlobalState[groupId].counter = -1;
+                break;
+            }
+
             GlobalState[groupId].counter -= 1;
+
             break;
     }
 }
@@ -115,10 +138,28 @@ function getExtensionActive(groupId: string): boolean {
 // Helpers
 
 function getOrRegisterExtension(): seal.ExtInfo {
-  let ext = seal.ext.find(EXT_NAME);
-  if (!ext) {
-    ext = seal.ext.new(EXT_NAME, EXT_AUTHOR, EXT_VERSION);
-    seal.ext.register(ext);
-  }
-  return ext;
+    let ext = seal.ext.find(EXT_NAME);
+
+    if (!ext) {
+        ext = seal.ext.new(EXT_NAME, EXT_AUTHOR, EXT_VERSION);
+        seal.ext.register(ext);
+        seal.ext.registerFloatConfig(ext, CONF_CHANCE_KEY, CONF_CHANCE_DEF, "每次收到消息时，将消息标记为复读的概率(大于等于 1 则必定触发)");
+    }
+
+    return ext;
+}
+
+function hasCqComponent(message: string): boolean {
+    if (message.length < 5) {
+        // impossible to contain a CQ component
+        return false;
+    }
+
+    // This will incorrectly match incomplete structures at the beginning,
+    // but we allow this behaviour as precision is not mandatory.
+    if (message.startsWith("[CQ:")) {
+        return true;
+    }
+
+    return message.match(`\[CQ:.+\]`) != null;
 }
